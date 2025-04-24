0f9d08211dae8203736fed084c6185909cb34ca9
Jonathan Huang <jonathanhuang@Jonathans-MacBook-Air.local>
2025-04-24 16:28:50 -0700

    modified for inqueries on client.py

--- client.py
+++ client.py
@@ -11,9 +11,30 @@ myTCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 myTCPSocket.connect((destination, port))
 
 while flag:
+    smallLoop = True
     
-    message = input("Message: ")
-    myTCPSocket.sendall(message.encode('utf-8'))
+    print(f"Please select from the following options: ")
+    
+    while smallLoop:
+        print(f"    1. The average moisture inside the kitchen fridge in the past three hours")
+        print(f"    2. The average water consumption per cycle in the smart dishwasher")
+        print(f"    3. The device that consumed more electricity among the three IoT devices (two refrigerators and a dishwasher)")
+        selection = input("Please enter your choice: ")
+        if selection == 1:
+            smallLoop = False
+            pass
+        elif selection == 2:
+            smallLoop = False
+            pass
+        elif selection == 3:
+            smallLoop = False
+            pass
+
+        else:
+            print(f"Sorry, this query cannot be processed. Please try one of the following:")
+
+
+    myTCPSocket.sendall(selection.encode('utf-8'))
     serverReponse = myTCPSocket.recv(1024)
 
     print(f"Server Response: {serverReponse.decode()}")
